9e3fbab2949cb000b7238fe1741446462bce7d66
ywyii123 <1524039257@qq.com>
2024-04-23 06:13:45 +0000

    2

--- install_and_train.sh
+++ install_and_train.sh
@@ -3,7 +3,6 @@ python setup.py develop
 
 ln -s /root/data_atd/ImageNet ./
 
-
 python -m torch.distributed.launch \
     --use-env \
     --nproc_per_node=4 \
@@ -11,3 +10,10 @@ python -m torch.distributed.launch \
     basicsr/train.py \
     -opt options/train/EDSR_CT_SRx4.yml \
     --launcher pytorch
+
+current_date=$(date +%Y%m%d)
+rundir_logs="${current_date}_tb_loggers"
+rundir_experiments="${current_date}_experiments"
+
+cp -rf experiments/ /root/data_atd/$rundir_experiments
+cp -rf tb_loggers/ /root/data_atd/$rundir_logs
